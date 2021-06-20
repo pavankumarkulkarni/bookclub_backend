fc63a6eed8b12ad2abe214d8e6c338d2bd535257
Pavan kumar Kulkarni <pavankumar.kulkarni@gmail.com>
2021-06-19 22:59:28 -0400

    addBookCopy and addReview resolvers

--- graphQL/resolvers/bookResolver.js
+++ graphQL/resolvers/bookResolver.js
@@ -31,9 +31,49 @@ const bookResolvers = {
         const bookInDB = await bookSaved.save();
         const userUpdated = await userCollection.findOneAndUpdate(
           { _id: me.id },
-          { $push: { booksOwned: { bookID: bookInDB._id, available: true } } }
+          { $push: { booksOwned: { bookID: bookInDB._id, available: true } } },
+          { new: true }
         );
         return bookInDB;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
+
+    reviewBook: async (
+      _,
+      { input },
+      { bookCollection, userCollection, me }
+    ) => {
+      try {
+        if (!me) {
+          throw new Error("Login to review book!");
+        }
+        const { bookID, comment, rating } = input;
+        const bookInDB = await bookCollection.findById(bookID);
+        if (!bookInDB) {
+          throw new Error("Book not found!");
+        }
+        const currRating = bookInDB.rating ? bookInDB.rating : 0;
+        const currReviewsCount = bookInDB.reviews.length;
+        const newRating =
+          Math.round(
+            ((currRating * currReviewsCount + rating) /
+              (currReviewsCount + 1)) *
+              10
+          ) / 10;
+
+        const bookUpdated = await bookCollection.findOneAndUpdate(
+          { _id: bookID },
+          {
+            $set: { rating: newRating },
+            $push: {
+              reviews: { userID: me.id, comment: comment, rating: rating },
+            },
+          },
+          { new: true }
+        );
+        return bookUpdated;
       } catch (err) {
         throw new Error(err);
       }
